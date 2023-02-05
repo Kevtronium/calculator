3143a733e1081f1a60f0b5759bf75fde42fdbf2d
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2023-02-05 14:38:40 -0800

    Implement operate() function

--- js/index.js
+++ js/index.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  let result = 0;
+
+  switch (operator) {
+    case '+':
+      result = add(num1, num2);
+      break;
+    case '-':
+      result = subtract(num1, num2);
+      break;
+    case '*':
+      result = multiply(num1, num2);
+      break;
+    default:
+      result = divide(num1, num2);
+  }
+  return result;
+}
